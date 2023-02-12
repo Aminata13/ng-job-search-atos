538e7d138e70bbd2139d98e205cc1f33fb2b70b4
Aminata BA <aminata.ba@atos.net>
2023-02-12 12:58:53 +0000

    added project, architecture set up

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { ModulesModule } from './modules/modules.module';
+import { PublicModule } from './public/public.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    PublicModule,
+    CoreModule,
+    SharedModule,
+    ModulesModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
